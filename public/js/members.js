d0053a596a252ecff6bc5153b7cf5f603313a618
Sederick Cowart <sederickbc@gmail.com>
2020-06-16 18:25:32 -0700

    update to client-side rendering card-info-pic

--- public/js/members.js
+++ public/js/members.js
@@ -5,10 +5,50 @@ $(document).ready(() => {
   $.get("/api/user_data").then(data => {
     currentUser = data;
     console.log(currentUser);
+    renderCards(currentUser.players);
     $(".member-name").text(data.email);
   });
 
-  $.get("/");
+  function renderCards(players) {
+    for (let i = 0; i < players.length; i++) {
+      $(`#startImg${i}`).html(`
+      <img class="img-fluid" src="https://media.gettyimages.com/photos/feb-2000-steve-francis-of-the-houston-rockets-jumps-to-the-basket-picture-id72484004?s=612x612"/>
+      `);
+      $(`#start${i}`).html(`
+        <div class="row">${players[i].PLAYER}</div>
+        <div class="row">
+        <div class="col-3">
+        <div class="row">GP</div>
+        <div class="row">PTS</div>
+        <div class="row">FG%</div>
+        <div class="row">3P%</div>
+        <div class="row">FT%</div>
+        </div>
+        <div class="col-3">
+        <div class="row">${players[i].GP}</div>
+        <div class="row">${players[i].PTS}</div>
+        <div class="row">${players[i]["FG%"]}</div>
+        <div class="row">${players[i]["3P%"]}</div>
+        <div class="row">${players[i]["FT%"]}</div>
+        </div>
+  <div class="col-3">
+    <div class="row">REB</div>
+    <div class="row">AST</div>
+    <div class="row">STL</div>
+    <div class="row">BLK</div>
+    <div class="row">TOV</div>
+  </div>
+  <div class="col-3">
+    <div class="row">${players[i].REB}</div>
+    <div class="row">${players[i].AST}</div>
+    <div class="row">${players[i].STL}</div>
+    <div class="row">${players[i].BLK}</div>
+    <div class="row">${players[i].TOV}</div>
+  </>
+</div>
+      `);
+    }
+  }
 
   $(".fa-heart").on("click", function() {
     const fav = $(this).data("fav");
