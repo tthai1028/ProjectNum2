7cdc5022fde6e0b492187b85e3935ed553495e37
tthai1028 <61211397+tthai1028@users.noreply.github.com>
2020-06-18 05:03:45 -0700

    Update html-routes.js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,7 +6,7 @@ const db = require("../models");
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
-  app.get("/", (req, res) => {
+  app.get("*", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
