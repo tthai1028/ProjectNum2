b241a5c75d276d16842ca00426e0f39dc44a29f9
Thomas <tthai4@gmail.com>
2020-06-18 19:13:47 -0700

    fnal x8

--- utils/seeder.js
+++ utils/seeder.js
@@ -5,10 +5,12 @@ const path = "../db/Start5_db.csv";
 const runSeed = async () => {
   try {
     const seeds = await csv().fromFile(path);
+    console.log("seeds", seeds);
     const mapped = seeds.map(a => {
       delete a["#"];
       return a;
     });
+    console.log("mapped", mapped);
     await db.Player.bulkCreate(mapped);
   } catch (err) {
     console.log("run seed", err);
